c65335ac4ac5f0a251994af6c38bb270930f8ecc
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-11 14:12:14 +0200

    Change models deadline -> forma date.

--- todocore/models.py
+++ todocore/models.py
@@ -11,7 +11,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateField(blank=True, null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks", blank=True, null=True)
 
