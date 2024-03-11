061c20a303966a8e2213b9358d13dbe162742044
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-11 14:13:05 +0200

    Add views

--- todocore/views.py
+++ todocore/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
-# Create your views here.
+from todocore.forms import TaskForm
+from todocore.models import Task, Tag
+
+
+class TaskListView(ListView):
+    model = Task
+    ordering = ["is_done"]
+    paginate_by = 8
+    template_name = "todocore/index.html"
+
+
+def task_make_is_done(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_done = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy(viewname="index"))
+
+
+def task_make_is_not_dane(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_done = False
+    task.save()
+    return HttpResponseRedirect(reverse_lazy(viewname="index"))
+
+
+def task_delete(request, pk):
+    Task.objects.get(id=pk).delete()
+    return HttpResponseRedirect(reverse_lazy(viewname="index"))
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("index")
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("index")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = "index"
+
+
+def tag_delete(request, pk):
+    Tag.objects.get(id=pk).delete()
+    return HttpResponseRedirect(reverse_lazy("tags-list"))
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tags-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tags-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tags-list")
+
+
+class TagListView(ListView):
+    model = Tag
+    paginate_by = 8
