2707615c5697a5ac9a9d7b8d7be7721f03c148cc
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-11 14:12:42 +0200

    Add unique attrs for name Tag

--- todocore/models.py
+++ todocore/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
