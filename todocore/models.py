f71b65c09dcfdb9207b23639ca014e7986ec1e3e
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-11 10:49:50 +0200

    Create models Tag, Task

--- todocore/models.py
+++ todocore/models.py
@@ -1,3 +1,21 @@
+from time import timezone
+
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name='tasks')
+
+    def __str__(self):
+        return self.content
