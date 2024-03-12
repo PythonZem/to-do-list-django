c8ab0d26553204498a00b04568cf7da609c95757
Andrii Zemliannikov <pythonzem@gmail.com>
2024-03-12 09:41:55 +0200

    fix URLs models/pk/action

--- todocore/urls.py
+++ todocore/urls.py
@@ -16,12 +16,12 @@ from todocore.views import (
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/is_done/<int:pk>", task_make_is_done, name="task-is-done"),
-    path("task/is_not_done/<int:pk>", task_make_is_not_dane, name="task-is-not-done"),
-    path("task/update/<int:pk>", TaskUpdateView.as_view(), name="task-update"),
-    path("task/delete/<int:pk>", task_delete, name="task-delete"),
+    path("task/<int:pk>/is_done/", task_make_is_done, name="task-is-done"),
+    path("task/<int:pk>/is_not_done/", task_make_is_not_dane, name="task-is-not-done"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", task_delete, name="task-delete"),
     path("tags/", TagListView.as_view(), name="tags-list"),
     path("tags/create/", TagCreateView.as_view(), name="tags-create"),
-    path("tags/update/<int:pk>", TagUpdateView.as_view(), name="tags-update"),
-    path("tags/delete/<int:pk>", tag_delete, name="tags-delete"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tags-update"),
+    path("tags/<int:pk>/delete/", tag_delete, name="tags-delete"),
 ]
